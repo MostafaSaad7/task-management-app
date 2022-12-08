b0de9f750ed804df28e889fc3c276e92f919bf44
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-12-08 17:10:54 +0200

    Add global error handler - Modify  app.js - Modify  controllers/errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,6 +6,7 @@ const handleDuplicateFieldsDB = err => {
   const message = `Duplicate field value ${value} please use another value `;
   return new AppError(message, 400);
 };
+
 const handleJWTError = () =>
   new AppError('Invalid token please log in again.', 401);
 
@@ -16,17 +17,20 @@ const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value} `;
   return new AppError(message, 400);
 };
+
 const handleValidationErrorDB = err => {
   // eslint-disable-next-line node/no-unsupported-features/es-builtins
   const errors = Object.values(err.errors).map(el => el.message);
-  const message = `Invalid imput data ${errors.join('. ')}`;
+  const message = `Invalid input data ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
+
 const sendErrorProd = (err, response) => {
   if (err.isOperational) {
-    response
-      .status(err.status)
-      .json({ status: err.status, message: err.message });
+    response.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
   } else {
     console.error('Error 💥 ', err);
     response.status(500).json({
@@ -44,18 +48,26 @@ const sendErrorDev = (err, response) => {
     stack: err.stack
   });
 };
+
 module.exports = (err, request, response, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, response);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+
     if (err.name === 'CastError') error = handleCastErrorDB(err);
+
     if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+
     if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+
     if (err.name === 'JsonWebTokenError') error = handleJWTError();
+
     if (err.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
     sendErrorProd(error, response);
   }
 };
