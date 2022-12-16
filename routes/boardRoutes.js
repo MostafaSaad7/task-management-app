eec170bffcec2df7dd26e88422fe7282f8a811fc
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-12-16 23:08:18 +0200

    Enable nested route which includes user and boards. - Modify controllers/boardController.js by initiating members and owners for a board. - Modify routes/boardRoutes.js to enable parent parameter merging and set owners and members. - Modify routes/userRoutes.js to enable nested routes.

--- routes/boardRoutes.js
+++ routes/boardRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const boardController = require('../controllers/boardController');
 const columnRouter = require('./columnRoutes');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 router.use('/:boardId/columns', columnRouter);
 
 router
   .route('/')
   .get(boardController.getAllBoards)
-  .post(boardController.createBoard);
+  .post(boardController.setOwnersMembersId, boardController.createBoard);
 
 router
   .route('/:id')
