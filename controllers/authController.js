e6a0cf3d6c5e524f6c1d5f0475ef4c163a059eb2
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-12-17 16:50:36 +0200

    Implement signup function - Modify controllers/authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+    // secure: req.secure
+  });
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
+const signup = catchAsync(async (request, response, next) => {
+  // Mongo errors handled
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirm: request.body.passwordConfirm
+  });
+  createSendToken(newUser, 201, request, response);
+});
+
+
+module.exports = {
+  login,
+  signup,
+  protect,
+  restrictTo,
+  resetPassword,
+  forgotPassword,
+  updatePassword
+};
